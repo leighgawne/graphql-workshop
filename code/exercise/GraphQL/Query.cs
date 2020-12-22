84013f3ffd4ef9232e1080643932f05740b46b53
Leigh Gawne <leighgawne@gmail.com>
2020-12-22 10:17:52 +0000

    Graphql exercise

---
+++ code/exercise/GraphQL/Query.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HotChocolate;
+using ConferencePlanner.GraphQL.Data;
+
+namespace GraphQL
+{
+    public class Query
+    {
+        public IQueryable<Speaker> GetSpeakers([Service] ApplicationDbContext context) =>
+            context.Speakers;
+    }
+}
